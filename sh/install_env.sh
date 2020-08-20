bce6beeb3df015e97596d28ac631836f27f47330
purpleapples <root@localhost.localdomain>
2020-08-20 14:14:03 +0900

    java

---
+++ sh/install_env.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# object : to install new environment on the base
+
+# procedure 
+# 1. yum update
+# 2. install rdate, cronie, gcc, gcc-c++, make, cmake, psimc, bind-utils, net-tools, wget
+# 3. set the time equal to a specific one. url : time.bora.net
+# 4. install curl-devel for other network protocol
+# 5. add user for mysql, web development
+# 6. modify profile : add timeout, history format
+
